609a83d54bfc480b7ce6f5b1ac4ae7a0fb219b53
ASRORBEK <asrorbekeraliyev@gmail.com>
2022-08-06 14:29:50 +0500

    First created status

---
+++ src/main/java/uz/tech4ecobackend/Tech4ecoBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.tech4ecobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tech4ecoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tech4ecoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/tech4ecobackend/Tech4ecoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.tech4ecobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tech4ecoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
